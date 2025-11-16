a6e3dd7cc5b9597125b0e28856e1d194305f7b60
Vipul Jain <vipuljain675@gmail.com>
2025-11-16 21:51:30 +0530

    commit

--- Todo-app updated /src/store/todo-items-store.jsx
+++ Todo-app updated /src/store/todo-items-store.jsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
 export const TodoItemsContext = createContext({
-  todoItems,
-  addNewItem,
-  deleteItem,
+  todoItems: [],
+  addNewItem: () => {},
+  deleteItem: () => {},
 });
